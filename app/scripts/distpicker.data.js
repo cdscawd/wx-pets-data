0a3854fd8a7534ad057a0f1b3cc7914b2156e923
dsfsfrec <gatsbychao@gmail.com>
2018-02-03 12:41:54 +0800

    1

---
+++ app/scripts/distpicker.data.js
@@ -0,0 +1,278 @@
+
+// china addr
+var ChineseDistricts = [
+  {
+    "label": "北京市", 
+    "value": 0, 
+    "addr": "110000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "天津市", 
+    "value": 0, 
+    "addr": "120000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "河北省", 
+    "value": 0, 
+    "addr": "130000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "山西省", 
+    "value": 0, 
+    "addr": "140000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "内蒙古自治区", 
+    "value": 0, 
+    "addr": "150000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "辽宁省", 
+    "value": 0, 
+    "addr": "210000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "吉林省", 
+    "value": 0, 
+    "addr": "220000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "黑龙江省", 
+    "value": 0, 
+    "addr": "230000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "上海市", 
+    "value": 0, 
+    "addr": "310000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "江苏省", 
+    "value": 0, 
+    "addr": "320000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "浙江省", 
+    "value": 0, 
+    "addr": "330000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "安徽省", 
+    "value": 0, 
+    "addr": "340000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "福建省", 
+    "value": 0, 
+    "addr": "350000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "江西省", 
+    "value": 0, 
+    "addr": "360000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "山东省", 
+    "value": 0, 
+    "addr": "370000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "河南省", 
+    "value": 0, 
+    "addr": "410000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "湖北省", 
+    "value": 0, 
+    "addr": "420000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "湖南省", 
+    "value": 0, 
+    "addr": "430000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "广东省", 
+    "value": 0, 
+    "addr": "440000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "广西壮族自治区", 
+    "value": 0, 
+    "addr": "450000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "海南省", 
+    "value": 0, 
+    "addr": "460000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "重庆市", 
+    "value": 0, 
+    "addr": "500000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "四川省", 
+    "value": 0, 
+    "addr": "510000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "贵州省", 
+    "value": 0, 
+    "addr": "520000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "云南省", 
+    "value": 0, 
+    "addr": "530000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "西藏自治区", 
+    "value": 0, 
+    "addr": "540000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "陕西省", 
+    "value": 0, 
+    "addr": "610000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "甘肃省", 
+    "value": 0, 
+    "addr": "620000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "青海省", 
+    "value": 0, 
+    "addr": "630000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "宁夏回族自治区", 
+    "value": 0, 
+    "addr": "640000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "新疆维吾尔自治区", 
+    "value": 0, 
+    "addr": "650000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "台湾省", 
+    "value": 0, 
+    "addr": "710000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "香港特别行政区", 
+    "value": 0, 
+    "addr": "810000", 
+    "children": [
+      
+    ],
+  }, 
+  {
+    "label": "澳门特别行政区", 
+    "value": 0, 
+    "addr": "820000", 
+    "children": [
+      
+    ],
+  }
+]
+
+
