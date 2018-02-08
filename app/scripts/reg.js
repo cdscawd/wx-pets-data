7f103868af387965511daf7d722bb1cb224aa7ed
dsfsfrec <gatsbychao@gmail.com>
2018-02-08 23:23:22 +0800

    1

--- app/scripts/reg.js
+++ app/scripts/reg.js
@@ -1,8 +1,17 @@
 
 
 var ERR_TEXT = {
-	Phone_Number: '此账号已被注册，请返回上页重新选择，若忘记密码，可到巧口萌宠App短信验证找回',
+	Phone_Number_Error: '填写正确的手机号',
+	Phone_Number_Registar: '此账号已被注册，请返回上页重新选择，若忘记密码可到巧口萌宠App短信验证找回',
 	Verification_Code: '请填写正确的验证码',
+	Password_Error: '请设置密码',
+	Password_Differ: '密码设置不一致，请重新设置',
+	Account_Error: '未查询到此账号信息，请检查账号与密码是否填写正确或回到上页重新选择',
+	Baby_Name_Empty: '请填写宝贝姓名',
+	Baby_Brithday_Empty: '请选择宝贝生日',
+	Activation_Error: '请填写正确的卡密激活码',
+	Address_Empty: '请完整选择省、市、区',
+	Address_Info_Empty: '请填写详细地址',
 }
 
 
@@ -18,12 +27,10 @@ function isPoneAvailable(text) {
 
 function ERRPrompt(type, input_value) {
 	switch (type) {
-		case 'PHONE_NUMBER':
+		case 'PHONE_NUMBER_ERROR':
 			if( !isPoneAvailable(input_value) ) {
-				$('#phone-input-box').after(`<p class="err-prompt">${ERR_TEXT.Phone_Number}</p>`)
+				$('#phone-input-box').after('<p class="err-prompt">' + ERR_TEXT.Phone_Number_Error + '</p>')
 			} 
-			break;
-		default:
 			break;
 	}	
 }
